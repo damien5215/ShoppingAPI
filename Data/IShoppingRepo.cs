f0453ac3ecb1bc2a7fe7ba233ebfad547b189491
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-07-23 16:01:39 +0100

    Initial commit

---
+++ Data/IShoppingRepo.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingAPI.Data
+{
+    public interface IShoppingRepo
+    {
+        IEnumerable<ShoppingList> GetFullList();
+        ShoppingList GetItemById(int id);
+    }
+}
---
+++ Data/MockShoppingRepo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShoppingAPI.Models;
+
+namespace ShoppingAPI.Data
+{
+    public class MockShoppingRepo : IShoppingRepo
+    {
+        public IEnumerable<ShoppingList> GetFullList()
+        {
+            var listOfItems = new List<ShoppingList>
+            {
+                new ShoppingList{
+                    Id = 0,
+                    ProductName = "Bread",
+                    Price = 1.10M,
+                    Quantity = 1,
+                    ShopName = "Sainsburies"
+                },
+                new ShoppingList{
+                    Id = 1,
+                    ProductName = "Milk",
+                    Price = 1.00M,
+                    Quantity = 2,
+                    ShopName = "M+S"
+                },
+                new ShoppingList{
+                    Id = 2,
+                    ProductName = "Pancakes",
+                    Price = 1.25M,
+                    Quantity = 2,
+                    ShopName = "M+S"
+                }
+
+            };
+            return listOfItems;
+        }
+
+        public ShoppingList GetItemById(int id)
+        {
+            return new ShoppingList { 
+                Id = 12, 
+                ProductName = "Soap",
+                Price = 1.00M,
+                Quantity = 1,
+                ShopName = "CostCo"
+            };
+        }
+    }
+}
