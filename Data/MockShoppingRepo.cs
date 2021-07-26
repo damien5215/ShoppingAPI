3d57e49c29d3f004e7448ec302119040d3d3a108
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-07-26 12:25:31 +0100

    Updated MockShoppingRepo

--- Data/MockShoppingRepo.cs
+++ Data/MockShoppingRepo.cs
@@ -8,45 +8,63 @@ namespace ShoppingAPI.Data
 {
     public class MockShoppingRepo : IShoppingRepo
     {
-        public IEnumerable<ShoppingList> GetFullList()
+        public IEnumerable<ShoppingList> CreateList() 
         {
-            var listOfItems = new List<ShoppingList>
+            List<ShoppingList> listOfItems = new List<ShoppingList>();
+
+            listOfItems.Add(new ShoppingList
             {
-                new ShoppingList{
-                    Id = 0,
-                    ProductName = "Bread",
-                    Price = 1.10M,
-                    Quantity = 1,
-                    ShopName = "Sainsburies"
-                },
-                new ShoppingList{
-                    Id = 1,
-                    ProductName = "Milk",
-                    Price = 1.00M,
-                    Quantity = 2,
-                    ShopName = "M+S"
-                },
-                new ShoppingList{
-                    Id = 2,
-                    ProductName = "Pancakes",
-                    Price = 1.25M,
-                    Quantity = 2,
-                    ShopName = "M+S"
-                }
+                Id = 0,
+                ProductName = "Bread",
+                Price = 1.10M,
+                Quantity = 1,
+                ShopName = "Sainsburies"
+            });
 
-            };
-            return listOfItems;
-        }
+            listOfItems.Add(new ShoppingList
+            {
+                Id = 1,
+                ProductName = "Milk",
+                Price = 1.00M,
+                Quantity = 2,
+                ShopName = "M+S"
+            });
 
-        public ShoppingList GetItemById(int id)
-        {
-            return new ShoppingList { 
-                Id = 12, 
+            listOfItems.Add(new ShoppingList
+            {
+                Id = 2,
+                ProductName = "Pancakes",
+                Price = 1.25M,
+                Quantity = 2,
+                ShopName = "M+S"
+            });
+
+            listOfItems.Add(new ShoppingList
+            {
+                Id = 3,
                 ProductName = "Soap",
                 Price = 1.00M,
                 Quantity = 1,
                 ShopName = "CostCo"
-            };
+            });
+
+            return listOfItems;
+        }
+
+        public IEnumerable<ShoppingList> GetFullList()
+        {
+            var list = CreateList();
+            
+            return list;
+        }
+
+        public ShoppingList GetItemById(int id)
+        {
+            var list = CreateList();
+
+            return list
+                .Where(b => b.Id == id)
+                .FirstOrDefault();
         }
     }
 }
