19c021d5de244f347e994ce2e31f325ceb295c3d
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-07-26 11:20:14 +0100

    Added Controller

---
+++ Controllers/ShoppingListsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingAPI.Models;
+using ShoppingAPI.Data;
+
+namespace ShoppingAPI.Controllers
+{
+    [Route("api/shoppinglists")]
+    [ApiController]
+    public class ShoppingListsController : ControllerBase
+    {
+        private readonly MockShoppingRepo _repository = new MockShoppingRepo();
+
+        //GET api/shoppinglist
+        [HttpGet]
+        public ActionResult<IEnumerable<ShoppingList>> GetAllItems()
+        {
+            var shoppingListItems = _repository.GetFullList();
+
+            return Ok(shoppingListItems);
+        
+        }
+
+        //GET api/shoppinglist/{id}
+        [HttpGet("{id}")]
+        public ActionResult <ShoppingList> GetItemById(int id)
+        {
+            var shoppingListItem = _repository.GetItemById(id);
+
+            return Ok(shoppingListItem);
+        }
+    }
+}
